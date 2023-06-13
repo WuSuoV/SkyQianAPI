e18da0df9ef0a062ad887128c8fcc58d025b50ae
勿埋我心 <skyqian@88.com>
2023-06-14 00:13:43 +0800

    Update jsonxasc.py

--- tools/jsonxasc.py
+++ tools/jsonxasc.py
@@ -4,4 +4,5 @@ from flask import Response
 
 
 def jsonxasc(adict: dict):
-    return Response(json.dumps(adict, ensure_ascii=False, sort_keys=False, indent=4), content_type='application/json;charset=utf-8')
+    return Response(json.dumps(adict, ensure_ascii=False, sort_keys=False, indent=4),
+                    content_type='application/json;charset=utf-8')
